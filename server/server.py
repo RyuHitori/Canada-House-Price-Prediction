e4e592f11867644b9ff81106b673795878a24155
Minh Tan To <tominhtan500@gmail.com>
2025-12-15 16:21:03 -0500

    server

--- server/server.py
+++ server/server.py
@@ -1,11 +1,36 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import util
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Home Price Prediction API is running!"
+    estimated_price = None
+    if request.method == "POST":
+        # Gather form data
+        data = {
+            "City": request.form["City"],
+            "Province": request.form["Province"],
+            "Bedrooms": int(request.form["Bedrooms"]),
+            "Bathrooms": int(request.form["Bathrooms"]),
+            "Acreage": float(request.form["Acreage"]),
+            "Property Type": request.form["Property_Type"],
+            "Square Footage": float(request.form["Square_Footage"]),
+            "Garage": request.form["Garage"],
+            "Parking": request.form["Parking"],
+            "Fireplace": request.form["Fireplace"],
+            "Heating": request.form["Heating"],
+            "Waterfront": request.form["Waterfront"],
+            "Sewer": request.form["Sewer"],
+            "Pool": request.form["Pool"],
+            "Garden": request.form["Garden"],
+            "Balcony": request.form["Balcony"]
+        }
+
+        # Predict
+        estimated_price = util.get_estimated_price(data)
+
+    return render_template("index.html", estimated_price=estimated_price)
 
 @app.route("/predict_home_price", methods=["POST"])
 def predict_home_price():
